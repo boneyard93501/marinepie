1d77dd0fda1fbf56fe75e19c5a1220741f9dc89f
boneyard93501 <4523011+boneyard93501@users.noreply.github.com>
2022-12-18 23:10:25 -0600

    update src

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use marine_rs_sdk::{marine, module_manifest, WasmLoggerBuilder};
+use rustpython as vm;
+
+module_manifest!();
+
+pub fn main() {
+    WasmLoggerBuilder::new().build().unwrap();
+}
+
+#[marine]
+fn hello_py() -> String {
+    vm::Interpreter::without_stdlib(Default::default()).enter(|vm| {
+        let scope = vm.new_scope_with_builtins();
+
+        let code_obj = vm
+            .compile(
+                r#"print("Hello World!")"#,
+                vm::compiler::Mode::Exec,
+                "<embedded>".to_owned(),
+            )
+            .map_err(|err| vm.new_syntax_error(&err))
+            .unwrap();
+
+        let result = vm.run_code_obj(code_obj, scope);
+        result.unwrap()
+    })
+}
